4049fe1c941505cd1a33649b04fde79571ed069b
Jhayes97 <49526650+Jhayes97@users.noreply.github.com>
2019-04-11 11:34:18 -0400

    Add files via upload

---
+++ Iter.py
@@ -0,0 +1,18 @@
+import hashlib
+import itertools
+import string
+
+
+saltPrefix = "FCC"
+saltSuffix = "Cyber"
+hashedPass = "1a37687f02790ebd6e1be5b211afdf67"
+
+chars = string.ascii_letters + string.digits
+for password_length in range(1,6):
+    for iteration in itertools.product(chars, repeat=password_length):
+        iteration = ''.join(iteration)
+        fullHash = hashlib.md5(saltPrefix+iteration+saltSuffix).hexdigest()
+        print fullHash ,iteration
+        if fullHash.lower() == hashedPass:
+            print "The password is: " + iteration
+            exit(1)
